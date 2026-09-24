fe44362f7ce253480c97d48029f7e7b37e3e8936
Mart2718 <mart2718r@gmail.com>
2026-09-24 16:53:46 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Copy your Supabase Project URL and publishable (anon) key here.
 // Never put your service_role key or password in this file.
-window.SUPABASE_URL = 'YOUR_PROJECT_URL';
-window.SUPABASE_ANON_KEY = 'YOUR_PUBLISHABLE_KEY';
+window.SUPABASE_URL = 'https://fexhtcfmepeyifwmghpn.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_IWM7LG93Uk5SzsIG9tgF0g_xb-8VsLX';
